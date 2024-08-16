69a43318d5aa013f2121bf8735a9da04e06598ba
Sau0908 <kashyapsaurabh0908@gmail.com>
2024-08-16 13:23:38 +0530

    Remove:Dependices

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import { Provider } from "react-redux";
 import AllRoutes from "./AllRoutes";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import store from "./component/utils/store";
 
 function App() {
   return (
     <Provider store={store}>
-      <ToastContainer />
       <AllRoutes />
     </Provider>
   );
